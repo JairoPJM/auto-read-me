75a6249ca3beb826f3393e1b149da16e46f8f18a
JairoPJM <Jairomacassi@gmail.com>
2023-01-05 16:19:11 -0500

    almost done

--- index.js
+++ index.js
@@ -3,19 +3,33 @@ const generateMarkdown=require("./utils/generateMarkdown")
 const inquirer=require("inquirer")
 const fs=require("fs")
 // TODO: Create an array of questions for user input
+console.log('\x1b[32m', 'This is a green message!');
+
 const questions = [
     {
         type:"input",
         name:"title",
-        message:"What do you want to name your title?"
+        message:"What do you want the title to be for your readme? \x1b[34mEx-Auto-Read-ME"
     },{
         type:"input",
-        name:"description",
-        message:"Enter what you are wanting in your description section"
+        name:"descriptionMot",
+        message:"What was your motivation for this project?"
+    },{
+        type:"input",
+        name:"descriptionBuild",
+        message:"why did you build this project?"
+    },{
+        type:"input",
+        name:"descriptionProblem",
+        message:"What problems does this application solve?"
+    },{
+        type:"input",
+        name:"descriptionLearn",
+        message:"What are some things you learned while working on the project"
     },{
         type:"input",
         name:"installation",
-        message:"Enter what you are wanting in your installation section"
+        message:"What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running"
     },{
         type:"input",
         name:"usage",
@@ -23,23 +37,22 @@ const questions = [
     },{
         type:"input",
         name:"credits",
-        message:"Enter who worked on the project"
+        message:"Enter who worked on the project \x1b[34mEx-John Doe"
     },{
         type:"list",
         name:"license",
         message:"Which license would you like to use?",
         choices:["MIT License","The Unlicense","Boost Software License 1.0","None"],
-        validate:(userLicense)=>{
-            // if(userLicense==="MIT License")
-        }
+        
+        
     },{
         type:"input",
         name:"questions",
-        message:"Enter your github"
+        message:"Enter your github \x1b[34mEx-JairoPJM"
     },{
         type:"input",
         name:"email",
-        message:"Enter your email"
+        message:"Enter your email \x1b[34mEx-Jairomacassi@gmail.com"
     }
 ];
 
